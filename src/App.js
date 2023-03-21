edc1423651da4a6724b73e29379e8bf22981278f
Sean <seanzzz@Seans-Mac-Studio.local>
2023-03-21 10:50:18 -0600

    redux add/remove cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
   return (
     <div className="App">
-      <Auth />
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
       {/* <Layout /> */}
     </div>
   );
